618504078dd17a98e8b3e138444e039ef8e32a52
Zander Moss <zander@mit.edu>
2016-01-21 20:07:27 -0500

    The distances used in the argument of the linear brightness function were incorrect. They have now been corrected. The two extra bar parameters now needed to run this code are: barlen -- the total length of the bar (in cm), and meniscus -- the distance from the uncoated tip of the bar to the meniscus of the coating solution (can be seen as a little line near the top of the bar where the coating begins).

--- gradient_fit.py
+++ gradient_fit.py
@@ -5,8 +5,7 @@ import scipy.optimize as opt
 import math
 
 filenames = [line.rstrip('\n') for line in open('fnames.txt')]
-
-
+plotnames=["Bottom","Top"]
 
 directory="/home/pinkpig/physics/labdata/lightguide_caen/processed/"
 
@@ -44,24 +43,28 @@ for fname in enumerate(filenames):
 
 
 class fitstuff(object):
-	def __init__(self,x,y,xerr,yerr,xf,bar):
+	def __init__(self,x,y,xerr,yerr,bar):
 		self.x=x
 		self.y=y
 		self.xerr=xerr
 		self.yerr=yerr
-		self.delta=[3.20,3.20,3.15,3.10] #(cm)
-		self.meniscus=[2.90,2.95,2.90,0.7] #(cm)
+		#self.barlen=[50.6,50.6,50.55,50.5] #(cm) #sbars
+		#self.meniscus=[2.9,2.95,2.9,0.7] #(cm)
+		#self.barlen=[50.8,50.25,51.1] #(cm) full dirtybar
+		#self.meniscus=[0,0,2.2] #(cm)
+		self.barlen=[51.0,50.85,50.50] #(cm)
+		self.meniscus=[2.7,3.1,3.2] #(cm)
 		self.bar=bar
-		self.xf=xf
 
 	def setbar(self,bar):
 		self.bar=bar
 	
 
 	def fitfuncforward(self,x,param):
-		return param[0]*math.exp(-1.0*param[1]*x)*(1.0+(x)*param[2])
+		return param[0]*math.exp(-1.0*param[1]*x)*(1.0+(x-self.meniscus[self.bar])*param[2])
 	def fitfuncbackward(self,x,param):
-		return param[0]*math.exp(-1.0*param[1]*x)*(1.0+(self.xf-x+self.delta[self.bar]-self.meniscus[self.bar])*param[2])
+		return param[3]*param[0]*math.exp(-1.0*param[1]*x)*(1.0+(self.barlen[self.bar]-self.meniscus[self.bar]-x)*param[2])
+	
 
 	def chi2(self,param):
 		chi2=0
@@ -81,30 +84,33 @@ edgecut=[2,-2]
 
 x=np.ones(17)*zdist+np.linspace(0,16,17)*step
 x=x[edgecut[0]:edgecut[1]]
-param=np.zeros(3)
+param=np.zeros(4)
 
 bounds=[]
 
 bounds.append((10.0**4,10.0**5))
-bounds.append((1.0/10000,1.0))
-bounds.append((1.0/10000,1.0))
+bounds.append((1.0/1000,1.0))
+bounds.append((1.0/10000,2.0))
+bounds.append((0.1,2.0))
 
 
 
 
-for bar in range(0,4):
-	param[0]=means[2*bar,1,1] #init guess
+for bar in range(0,len(filenames)/2):
+	param[0]=means[2*bar,1,0]*0.5 #init guess
 	#param[0]=4e+04 #init guess
-	param[1]=1.0/400
-	param[2]=1.0/10.0
+	param[1]=1.0/200
+	param[2]=0.005
+	param[3]=1.0
 
-	fit=fitstuff(x,means[2*bar:2*bar+2,:,edgecut[0]:edgecut[1]],xe[2*bar:2*bar+2,:,edgecut[0]:edgecut[1]],ye[2*bar:2*bar+2,:,edgecut[0]:edgecut[1]],x[-1],bar)
-	res=opt.minimize(fit.chi2,param,bounds=bounds,method='L-BFGS-B',tol=1e-13)
+	fit=fitstuff(x,means[2*bar:2*bar+2,:,edgecut[0]:edgecut[1]],xe[2*bar:2*bar+2,:,edgecut[0]:edgecut[1]],ye[2*bar:2*bar+2,:,edgecut[0]:edgecut[1]],bar)
+	res=opt.minimize(fit.chi2,param,bounds=bounds,method='L-BFGS-B',tol=1e-10)
 
 
 	print "CONST: ",res.x[0]
 	print "SLOPE: ",(1/res.x[1])
 	print "LINEAR GRADIENT: ",res.x[2]
+	print "COATING FACTOR: ",res.x[3]
 
 	yfuncf=np.zeros(len(x))
 	yfuncb=np.zeros(len(x))
@@ -120,11 +126,10 @@ for bar in range(0,4):
 	ax.errorbar(x,means[2*bar,1,edgecut[0]:edgecut[1]],xerr=xe[2*bar,1,edgecut[0]:edgecut[1]],yerr=ye[2*bar,1,edgecut[0]:edgecut[1]],fmt='ro',label='Data')
 	ax.plot(x,yfuncf,'b-',label='Fit')
 	#ax.plot(xodone,yfunc,'go')
-	ax.set_title("Sbar"+str(bar+1)+" Fits with Linear Gradient")
+	ax.set_title("KBar "+str(bar+1)+" with Linear Gradient")
 	ax.set_ylabel("Charge (ADC*TDC)")
 	legend = plt.legend(loc='lower right', shadow=False, fontsize='large')
-	ax.set_ylim([0,30000])
-	
+	ax.set_ylim(0,30000)	
 	ax2=plt.subplot(212)
 	#ax2.errorbar(x,means[2*bar+1,0,edgecut[0]:edgecut[1]],xerr=xe[2*bar+1,0,edgecut[0]:edgecut[1]],yerr=ye[2*bar+1,0,edgecut[0]:edgecut[1]],fmt='ro',label='')
 	ax2.errorbar(x,means[2*bar+1,1,edgecut[0]:edgecut[1]],xerr=xe[2*bar+1,1,edgecut[0]:edgecut[1]],yerr=ye[2*bar+1,1,edgecut[0]:edgecut[1]],fmt='ro',label='')
@@ -132,12 +137,13 @@ for bar in range(0,4):
 	ax2.set_xlabel("Illumination Distance from PMT (cm)")
 	ax2.set_ylabel("Charge (ADC*TDC)")
 	
+	ax2.set_ylim(0,30000)	
 	
 
-	ax2.text(0.65, -0.875, 'attenuation='+'{:04.1f}'.format(1/res.x[1])+' cm', style='italic',bbox={'facecolor':'cyan', 'alpha':0.5, 'pad':10},transform=ax.transAxes)
-	ax2.text(0.65, -1.05, 'brightness='+'{:02.1f}'.format(100*res.x[2])+' %/cm', style='italic',bbox={'facecolor':'cyan', 'alpha':0.5, 'pad':10},transform=ax.transAxes)
+	ax2.text(0.65, -0.35, 'attenuation='+'{:04.1f}'.format(1/res.x[1])+' cm', style='italic',bbox={'facecolor':'cyan', 'alpha':0.5, 'pad':10},transform=ax.transAxes)
+	ax2.text(0.65, -0.5, 'brightness='+'{:03.2f}'.format(100*res.x[2])+' %/cm', style='italic',bbox={'facecolor':'cyan', 'alpha':0.5, 'pad':10},transform=ax.transAxes)
+	ax2.text(0.65, -0.65, 'coating factor='+'{:03.2f}'.format(res.x[3]), style='italic',bbox={'facecolor':'cyan', 'alpha':0.5, 'pad':10},transform=ax.transAxes)
 
-	ax2.set_ylim([0,30000])
 	
 	plt.show()
 	
